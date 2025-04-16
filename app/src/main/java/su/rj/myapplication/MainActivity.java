7b6f21563b609cfefa04fde6df54bb226eafe121
rilskyjohny <rilskyjohny@gmail.com>
2025-04-16 18:54:23 +0300

    Move to Activity...Binding in MainActivity.

--- app/src/main/java/su/rj/myapplication/MainActivity.java
+++ app/src/main/java/su/rj/myapplication/MainActivity.java
@@ -9,19 +9,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import su.rj.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    Button button_question_mark,button_settings;
+    ActivityMainBinding amb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(amb.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        button_question_mark = findViewById(R.id.button);
-        button_settings = findViewById(R.id.button2);
     }
 }
