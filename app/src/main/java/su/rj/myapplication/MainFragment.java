5bdc23a3399d8c377fd29bdad5e04db89d720606
rilskyjohny <rilskyjohny@gmail.com>
2025-05-08 12:42:07 +0300

    Many logging.

--- app/src/main/java/su/rj/myapplication/MainFragment.java
+++ app/src/main/java/su/rj/myapplication/MainFragment.java
@@ -57,17 +57,25 @@ public class MainFragment extends Fragment
             sqoh = new SQLiteOpenHelper(context,dbName,null,1) {
                 @Override
                 public void onCreate(SQLiteDatabase db) {
+                    Log.d("Database","onCreate");
+                    Log.d("Database","Create table zakazi. ");
                     db.execSQL("CREATE TABLE IF NOT EXISTS "+
                             "zakazi(subid INTEGER PRIMARY KEY AUTOINCREMENT,"+
                             "id INTEGER,tovarid INTEGER,count INTEGER,creds TEXT)");
+                    Log.d("Database","Create table tovari. ");
                     db.execSQL("CREATE TABLE IF NOT EXISTS tovari(tovarid INTEGER PRIMARY KEY AUTOINCREMENT,"+
                             "name TEXT)");
                     db.execSQL("INSERT INTO tovari VALUES(?,?)", new Object[]{1, "Test"});
+                    Log.d("Database","Inaert test values into table tovari. ");
                     db.execSQL("INSERT INTO zakazi VALUES(?,?,?,?,?)", new Object[]{1,1,1,1,"Test"});
+                    Log.d("Database","Inaert test values into table zakazi. ");
                 }
                 @Override
                 public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+                    Log.d("Database","onUpgrade from "+oldVersion+" to "+newVersion);
+                    Log.d("Database","Drop table zakazi. ");
                     db.execSQL("DROP TABLE IF EXISTS zakazi");
+                    Log.d("Database","Drop table tovari. ");
                     db.execSQL("DROP TABLE IF EXISTS tovari");
                     this.onCreate(db);
                 }
@@ -75,14 +83,22 @@ public class MainFragment extends Fragment
             SQLiteDatabase db = sqoh.getReadableDatabase();
             Cursor cur = db.rawQuery("SELECT * from zakazi",null);
             do {
+                String s = "Column names:[";
+                for(String i:cur.getColumnNames()){
+                    s+=i;
+                    s+=", ";
+                }
+                s+="]";
+                Log.d("Load rqs",s);
                 generateSubRq(cur);
             } while (cur.moveToNext());
             for (Rq.SubRq subRq : subRqs) {
-                Log.d("Hi",subRq.toString());
+                Log.d("Load rqs","subRq:"+subRq.toString());
             }
             cur.close();
             throw new Exception("Hi");
         } catch (Exception t){
+            Log.e("Load rqs","Some error ocurred. ",t);
             ErrorFragment.scareUser(t,getContext(),requireActivity().getSupportFragmentManager());
         }
     }
